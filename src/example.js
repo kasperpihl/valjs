64ff7034c93fff6b36a639cc23b78ee5cd658c75
Kasper Pihl Tornøe <kasper@pihl.it>
2017-01-23 20:21:01 -0800

    Using funcwrap in example

--- src/example.js
+++ src/example.js
@@ -1,16 +1,25 @@
 import 'babel-polyfill';
-import val, { bool, string, number, array, object, any, date, func } from './index';
-val.setGlobal('log', true);
+import val, { bool, string, number, array, object, any, date, func, funcWrap } from './index';
 
-bool.require().test(false);
 
-any.require().test();
-console.log('test1');
-const testObj = {
-  schema: {},
-  middleware: null
-};
-const error = object.as({
-  schema: object.require(),
-  middleware: func,
-}).test(testObj);
+
+
+const deleteGoal = funcWrap([
+  string.require()
+], (err, id) => {
+  if(!err){
+    console.log('deleting: ' + id);
+  }
+});
+
+const addGoal = funcWrap([object.as({
+  id: string.require(),
+  title: string.require()
+})], (err, goal) => {
+  if(!err){
+    console.log('adding', goal);
+  }
+});
+
+addGoal({id: 'id1', 'title': 'hello'});
+deleteGoal('id1');
